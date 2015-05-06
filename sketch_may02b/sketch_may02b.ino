12760fbe420a61e3d9cffd2ba75cec39e30cb489
Kevin Phan <kevinphan1337@gmail.com>
2015-05-05 20:01:19 -0400

    I moved the code into the folder
    Arduino had an issue so I fixed it. I added in code for three more
    motors. I am doing a load test.

--- sketch_may02b/sketch_may02b.ino
+++ sketch_may02b/sketch_may02b.ino
@@ -9,6 +9,9 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 // You can also make another motor on port M2
 //Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
@@ -24,9 +27,22 @@ void setup() {
   myMotor->run(FORWARD);
   // turn on motor
   myMotor->run(RELEASE);
+  myMotor2->setSpeed(255);
+  myMotor2->run(FORWARD);
+  // turn on motor
+  myMotor2->run(RELEASE);
+  myMotor3->setSpeed(255);
+  myMotor3->run(FORWARD);
+  // turn on motor
+  myMotor3->run(RELEASE);
+  myMotor4->setSpeed(255);
+  myMotor4->run(FORWARD);
+  // turn on motor
+  myMotor4->run(RELEASE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 myMotor->run(FORWARD);
+myMotor2->run(FORWARD);
 }
