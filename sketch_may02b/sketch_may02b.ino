5e41e31964ca95a70c603e5fdf44575e8f95c4ae
Kevin Phan <kevinphan1337@gmail.com>
2015-05-18 14:00:23 -0400

    Changes in test

--- sketch_may02b/sketch_may02b.ino
+++ sketch_may02b/sketch_may02b.ino
@@ -9,9 +9,6 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 // You can also make another motor on port M2
 //Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
@@ -23,26 +20,13 @@ void setup() {
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(255);
+  myMotor->setSpeed(175);
   myMotor->run(FORWARD);
   // turn on motor
   myMotor->run(RELEASE);
-  myMotor2->setSpeed(255);
-  myMotor2->run(FORWARD);
-  // turn on motor
-  myMotor2->run(RELEASE);
-  myMotor3->setSpeed(255);
-  myMotor3->run(FORWARD);
-  // turn on motor
-  myMotor3->run(RELEASE);
-  myMotor4->setSpeed(255);
-  myMotor4->run(FORWARD);
-  // turn on motor
-  myMotor4->run(RELEASE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 myMotor->run(FORWARD);
-myMotor2->run(FORWARD);
 }
